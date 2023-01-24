237d2245439fce07102ad0d00619a1451d9b3a2a
adrianmetowski <metowskiadrian21@gmail.com>
2023-01-24 15:16:00 +0100

    working get info from json returned by riot api

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ except ImportError:
     from argparse import Namespace
 
 summoner_name = 'Psycho93PL'
-api_key = 'RGAPI-be3f4ea3-1846-4aea-8763-7d9239d42bb8'
+api_key = 'RGAPI-1273b844-609c-4210-91fa-2b04a589c024'
 summoner_api_url = 'https://eun1.api.riotgames.com/lol/summoner/v4/summoners/'
 matches_api_url = 'https://europe.api.riotgames.com/lol/match/v5/matches/'
 
@@ -33,39 +33,31 @@ def get_matches_id(puuid, api_key):
 
 match_ids = get_matches_id(puuid, api_key)
 
-
-def get_match_data(match_ids, api_key):
-    api_url = f'{matches_api_url}{match_ids[0]}/?api_key={api_key}'
-    response = requests.get(api_url)
-    json_object = response.json()
-    data = json.dumps(json_object, indent=3)
-    return data
-# def get_match_data(match_ids, api_key):
-#     match_data = []
-#     for ele in match_ids:
-#         api_url = f'{matches_api_url}{ele}/?api_key={api_key}'
-#         response = requests.get(api_url)
-#         json_object = response.json()
-#         match_data.append(json_object)
-#     data = json.dumps(match_data, indent=3)
-#     return data
-
-
-match_data = get_match_data(match_ids, api_key)
-
-
 def get_summ_info_by_puuid(puuid, match_data):
     data = json.loads(match_data, object_hook=lambda d: Namespace(**d))
     participants = data.info.participants
     for i in participants:
         if i.puuid == puuid:
             return i
-def get_summ_metadata_by_puuid(puuid, match_data):
-    data = json.loads(match_data, object_hook=lambda d: Namespace(**d))
-    metadata = data.metadata.participants
-    for i in metadata:
-        if i == puuid:
-            return data.metadata
+def get_match_data(match_ids, api_key):
+    match_data_list = []
+    for match in match_ids:
+        api_url = f'{matches_api_url}{match}/?api_key={api_key}'
+        response = requests.get(api_url)
+        json_object = response.json()
+        data = json.dumps(json_object, indent=2)
+        extracted_summ_info = get_summ_info_by_puuid(puuid, data)
+        match_data_list.append(extracted_summ_info.win)
+    return match_data_list
 
-get_my_summoner_info = get_summ_info_by_puuid(puuid, match_data)
-get_my_summoner_metadata = get_summ_metadata_by_puuid(puuid, match_data)
+
+match_data = get_match_data(match_ids, api_key)
+
+print(match_data)
+
+# def get_summ_metadata_by_puuid(puuid, match_data):
+#     data = json.loads(match_data, object_hook=lambda d: Namespace(**d))
+#     metadata = data.metadata.participants
+#     for i in metadata:
+#         if i == puuid:
+#             return data.metadata
